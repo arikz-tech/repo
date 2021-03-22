c8da60dbe0851b1492a72948baccad1a514ef34e
rostik36 <rostikwow25@gmail.com>
2021-03-22 18:46:19 +0200

    dfd

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,44 +1,74 @@
+/**
+ * @author Rostik
+ */
 public class SharedData 
 {
-	private int [] array;
+	
+	private ArrayList<Integer> array;
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
 	
-	public SharedData(int[] array, int b) {
+	/**
+	 * @param array - list of integers
+	 * @param b - list size
+	 */
+	public SharedData(ArrayList<Integer> array, int b) {
 		
 		this.array = array;
 		this.b = b;
 	}
 
+	/**
+	 * function to pull the win array
+	 * @return array with boolean varibles
+	 */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
 
+	/**
+	 * function to set win array
+	 * @param winArray - array containig varibels indicaes the winning of losing
+	 */
 	public void setWinArray(boolean [] winArray) 
 	{
 		this.winArray = winArray;
 	}
 
-	//ArrayList
-	public int[] getArray() 
+	/**
+	 * function to pull the array
+	 * @return the list of integers
+	 */
+	public ArrayList<integer> getArray() 
 	{
 		return array;
 	}
 
+	/**
+	 * function to pull the number of elements in the list
+	 * @return number of elements in the list(int)
+	 */
 	public int getB() 
 	{
 		return b;
 	}
 
+	/**
+	 * function to pull flag
+	 * @return flag indicator(boolean)
+	 */
 	public boolean getFlag() 
 	{
 		return flag;
 	}
 
+	/**
+	 * function to set the flag
+	 * @param new flag indicator( boolean)
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
-
 }
